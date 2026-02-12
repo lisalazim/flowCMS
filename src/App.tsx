04c31792bbef79c3765d6f338fbcdd3926be2ddb
lisastyles.css <lisalazi21@gmail.com>
2026-02-12 16:36:36 +0700

    feat: add responsive mobile navbar with hamburger menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,49 @@
 import './App.css'
+import { useState } from 'react';
+import { IoMenu, IoCloseOutline } from "react-icons/io5";
+
+
 
 function App() {
-
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <nav className='md:flex justify-between md:text-center absolute w-full top-0 p-6 md:px-20 bg-transparent'>
-        <p className='font-bold text-lg md:text-xl text-white'>FlowCMS</p>
-        <ul className='flex justify-between gap-4 md:gap-12 text-white'>
-          <a href=""><li className='text-base md:text-lg font-normal hover:text-blue-300'>Home</li></a>
-          <a href=""><li className='text-base md:text-lg font-normal hover:text-blue-300'>Features</li></a>
-          <a href=""><li className='text-base md:text-lg font-normal hover:text-blue-300'>Recent Posts</li></a>
-        </ul>
-        
+      <nav className="fixed top-0 w-full p-6 md:px-20 z-50 bg-transparent">
+        <div className="flex justify-between items-center">
+
+          {/* Logo */}
+          <p className={`font-bold text-xl transition ${open ? "text-blue-600" : "text-white"}`}>FlowCMS</p>
+
+          {/* Toggle button */}
+          <button
+            className={`md:hidden text-3xl z-50 cursor-pointer transition ${
+              open ? "text-blue-600" : "text-white"
+            }`}
+            onClick={() => setOpen(!open)}>
+            {open ? <IoCloseOutline /> : <IoMenu />}
+          </button>
+
+          {/* Desktop menu */}
+          <div className="hidden md:flex gap-12 text-white font-medium">
+            <a className="hover:text-blue-300 transition" href="">Home</a>
+            <a className="hover:text-blue-300 transition" href="">Features</a>
+            <a className="hover:text-blue-300 transition" href="">Recent Posts</a>
+          </div>
+
+        </div>
       </nav>
+
+      {/* Mobile fullscreen menu */}
+      <div
+        className={`fixed inset-0 bg-white md:hidden transition-all duration-300 z-40
+        ${open ? "flex" : "hidden"} flex-col justify-start items-start pt-32 pl-8 gap-8`}
+      >
+        <a onClick={() => setOpen(false)} className="text-xl hover:text-blue-600 transition" href="">Home</a>
+        <a onClick={() => setOpen(false)} className="text-xl hover:text-blue-600 transition" href="">Features</a>
+        <a onClick={() => setOpen(false)} className="text-xl hover:text-blue-600 transition" href="">Recent Posts</a>
+      </div>
+
+      {/* hero section */}
       <section className="hero-section flex justify-center items-center bg-[url('./assets/hero.png')] h-screen bg-cover bg-center font-(--font-primary)" >
         <div className="hero-content flex flex-col justify-center items-center text-center text-white px-4 md:px-20 lg:px-40">
           <h1 className="text-3xl md:text-6xl font-bold mb-4">Manage Your Content. <br />Ship Faster. Stay Organized.</h1>
